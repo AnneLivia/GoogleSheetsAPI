082fea9e000f88984a13dc9167a6f3be8f99d4e3
Anne Livia <annelivia16@gmail.com>
2022-06-01 15:05:54 -0300

    working with spreadsheets api

---
+++ index.js
@@ -0,0 +1,29 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import credentials from './configs/spreadsheet.js';
+
+const id = "1yt5AP6_wbBKEc868iQtJRC3zxB4b8gHRpiON8eendn8";
+
+const doc = new GoogleSpreadsheet(id);
+
+await doc.useServiceAccountAuth(credentials, err => {
+    console.log(err);
+});
+
+await doc.loadInfo();
+
+const worksheet = await doc.sheetsByIndex[0];
+const rows = await worksheet.getRows();
+
+rows.forEach( (row, index) => {
+   console.log(row.Questions, row.Responses);
+})
+
+// adding new data. Can create a google function that updates
+await worksheet.addRow({"Questions" : "How to get there?", "Responses" : "Just turn right!"})
+
+// can exclude rows
+rows.forEach( (row) => {
+    if (row.Questions === 'How to get there?')
+       row.delete();
+})
+
